63596d01d5fb12b823c5ea8df655a33b130ddfd3
BayAcc0103 <145135982+BayAcc0103@users.noreply.github.com>
2025-11-17 17:35:47 +0700

    fix category student

--- BlazingQuiz.Web/BlazingQuiz.Shared/QuizState.cs
+++ BlazingQuiz.Web/BlazingQuiz.Shared/QuizState.cs
@@ -6,9 +6,10 @@ namespace BlazingQuiz.Shared
     {
         public int StudentQuizId { get; private set; }
         public QuizListDto? Quiz { get; private set; }
-        public void StartQuiz(QuizListDto? quiz, int studentQuizId) => 
+        public int SelectedCategoryId { get; set; } = 0;
+        public void StartQuiz(QuizListDto? quiz, int studentQuizId) =>
             (Quiz, StudentQuizId) = (quiz, studentQuizId);
-        public void StopQuiz() => 
+        public void StopQuiz() =>
             (Quiz, StudentQuizId) = (null, 0);
     }
 }
