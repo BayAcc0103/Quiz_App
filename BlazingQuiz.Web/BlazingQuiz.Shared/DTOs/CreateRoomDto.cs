6eb954e55b8358c8d72c62e4a6b2e8af75a40ddb
BayAcc0103 <145135982+BayAcc0103@users.noreply.github.com>
2025-11-25 16:59:32 +0700

    fix ui create room/join room role student teacher

--- BlazingQuiz.Web/BlazingQuiz.Shared/DTOs/CreateRoomDto.cs
+++ BlazingQuiz.Web/BlazingQuiz.Shared/DTOs/CreateRoomDto.cs
@@ -11,8 +11,9 @@ namespace BlazingQuiz.Shared.DTOs
         [StringLength(500)]
         public string? Description { get; set; }
 
+        [Range(1, 50, ErrorMessage = "Maximum players must be between 1 and 50.")]
         public int MaxParticipants { get; set; } = 50;
-        
+
         public Guid? QuizId { get; set; } // Selected quiz for the room
     }
 }
