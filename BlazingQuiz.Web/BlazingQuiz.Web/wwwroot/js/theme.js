978eafcc5ccf6da90bffa9ae0081fd82a2fa8866
BayAcc0103 <145135982+BayAcc0103@users.noreply.github.com>
2025-12-08 23:33:39 +0700

    remove bg img

--- BlazingQuiz.Web/BlazingQuiz.Web/wwwroot/js/theme.js
+++ BlazingQuiz.Web/BlazingQuiz.Web/wwwroot/js/theme.js
@@ -3,11 +3,11 @@ window.themeUtils = {
     setTheme: function(theme) {
         document.documentElement.classList.toggle('dark-theme', theme === 'dark');
         document.documentElement.classList.toggle('light-theme', theme === 'light');
-        
+        ///asset/background/dark/bg-dark5.jpg
         // Set background based on theme
         const backgroundImage = theme === 'dark' 
-            ? 'url(/asset/background/dark/bg-dark5.jpg)' 
-            : 'url(/asset/background/light/bg-light5.jpg)';
+            ? 'none' 
+            : 'none';
             
         document.body.style.backgroundImage = backgroundImage;
         document.body.style.backgroundSize = 'cover';
